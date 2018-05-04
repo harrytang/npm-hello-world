5a0b08000ae77d9ff96a504dd915e99a64814902
Harry Tang <harry@powerkernel.com>
2018-05-04 12:29:10 +0700

    add files

---
+++ index.js
@@ -0,0 +1,8 @@
+/**
+ * @author Harry Tang <harry@powerkernel.com>
+ * @link https://powerkernel.com
+ * @copyright Copyright (c) 2018 Power Kernel
+ */
+exports.myPackageLog = function() {
+    console.log("logged from hello-world");
+};
